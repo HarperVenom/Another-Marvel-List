3abd2491ed082a26ee8754922acd9c6ac5846ae1
HarperVenom <162710380+HarperVenom@users.noreply.github.com>
2025-06-21 14:35:49 +0200

    grid

--- list.js
+++ list.js
@@ -1,3 +1,5 @@
+var overviewOpened = false;
+
 async function fillMovieList() {
   const response = await fetch("titles.json");
   const movies = await response.json();
@@ -7,25 +9,62 @@ async function fillMovieList() {
   movies.forEach((movie, index) => {
     const li = document.createElement("li");
     li.innerHTML = `
-        <img class="poster" src="${movie.poster}" alt="" />
-        <div class="info">
-          <h2 class="title">${index + 1}. ${movie.title}</h2>
-          <div class="details">
-            <h3 class="date"><span class="section">Date: </span>${formatDate(
-              movie.date
-            )}</h3>
-            <h3 class="duration"><span class="section">Duration: </span>${formatDuration(
-              movie.duration
-            )}</h3>
-            <p class="description"><span class="section">Description: </span>${
-              movie.description
-            }</p>
-          </div>
-        </div>
+        <img class="poster" src="posters/${movie.poster}" alt="" />
       `;
+
+    const img = li.querySelector(".poster");
+    img.addEventListener("click", () => {
+      // Check if the extra content already exists to avoid duplicates
+      // if (!li.querySelector(".extra-info")) {
+
+      if (overviewOpened) return;
+      overviewOpened = true;
+
+      document.body.classList.add("noscroll");
+
+      const overview = document.querySelector("#overview");
+      overview.classList.remove("hidden");
+
+      const overviewContainer = document.querySelector("#overview-container");
+      overviewContainer.innerHTML = `
+            <img class="poster" src="/posters/${movie.poster}" alt="" />
+            <div class="info">
+              <h2 class="title">${movie.title}</h2>
+
+              <div class="details">
+                <h3 class="date">
+                  <span class="section">Date: </span>${formatDate(movie.date)}
+                </h3>
+                <h3 class="duration">
+                  <span class="section">Duration: </span>${formatDuration(
+                    movie.duration
+                  )}
+                </h3>
+                <p class="description">
+                  <span class="section">Description: </span>${movie.description}
+                </p>
+              </div>
+            </div>
+          `;
+    });
+
     list.appendChild(li);
   });
 }
+
+// <div class="info">
+//           <h2 class="title">${index + 1}. ${movie.title}</h2>
+//           <div class="details">
+//             <h3 class="date"><span class="section">Date: </span>${formatDate(
+//               movie.date
+//             )}</h3>
+//             <h3 class="duration"><span class="section">Duration: </span>${formatDuration(
+//               movie.duration
+//             )}</h3>
+//             <p class="description"><span class="section">Description: </span>${
+//               movie.description
+//             }</p>
+//           </div>
 
 fillMovieList();
 
