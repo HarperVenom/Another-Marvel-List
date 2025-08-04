0efee6c8ffbf63264202779985695f64de1d5042
HarperVenom <162710380+HarperVenom@users.noreply.github.com>
2025-08-04 21:18:10 +0200

    seasons together

--- scripts/titles.js
+++ scripts/titles.js
@@ -19,47 +19,52 @@ function getAllTitlesReleaseOrder() {
   // Step 1: Flatten all entries
   for (const title of titles) {
     if (title.episodes) {
-      // To keep all season episodes together
-      // let episodes = [];
-      // for (let i = 0; i < title.episodes.length; i++) {
-      //   let ep = title.episodes[i];
-      //   episodes.push({
-      //     episodeIndex: i,
-      //     name: ep.name,
-      //     date: ep.date,
-      //     duration: ep.duration,
-      //   });
-      // }
-      // flattened.push({
-      //   type: "episodeBlock",
-      //   id: title.id,
-      //   name: title.name,
-      //   date: episodes[0].date,
-      //   commonData: {
-      //     description: title.description,
-      //     colors: title.colors,
-      //     links: title.links,
-      //   },
-      //   episodes: episodes,
-      //   lastEpisodeIndex: 0,
-      // });
+      let completeSeasons = true;
 
-      for (let i = 0; i < title.episodes.length; i++) {
-        const ep = title.episodes[i];
+      if (completeSeasons) {
+        // To keep all season episodes together
+        let episodes = [];
+        for (let i = 0; i < title.episodes.length; i++) {
+          let ep = title.episodes[i];
+          episodes.push({
+            seasonId: title.id,
+            episodeIndex: i,
+            name: ep.name,
+            date: ep.date,
+            duration: ep.duration,
+          });
+        }
         flattened.push({
-          type: "episode",
-          seasonId: title.id,
-          seasonName: title.name,
+          type: "episodeBlock",
+          id: title.id,
+          name: title.name,
+          date: episodes[episodes.length - 1].date,
           commonData: {
             description: title.description,
             colors: title.colors,
             links: title.links,
           },
-          episodeIndex: i,
-          name: ep.name,
-          date: ep.date,
-          duration: ep.duration,
+          episodes: episodes,
+          lastEpisodeIndex: 0,
         });
+      } else {
+        for (let i = 0; i < title.episodes.length; i++) {
+          const ep = title.episodes[i];
+          flattened.push({
+            type: "episode",
+            seasonId: title.id,
+            seasonName: title.name,
+            commonData: {
+              description: title.description,
+              colors: title.colors,
+              links: title.links,
+            },
+            episodeIndex: i,
+            name: ep.name,
+            date: ep.date,
+            duration: ep.duration,
+          });
+        }
       }
     } else {
       flattened.push({
@@ -71,7 +76,6 @@ function getAllTitlesReleaseOrder() {
         description: title.description,
         colors: title.colors,
         links: title.links,
-        split: title.split,
       });
     }
   }
@@ -83,44 +87,15 @@ function getAllTitlesReleaseOrder() {
   const grouped = [];
   let currentEpisodeBlock = null;
 
-  let hashedEpisodeBlocks = [];
-
   for (const item of flattened) {
     if (item.type === "episode") {
       if (
-        currentEpisodeBlock
-        // && currentEpisodeBlock.lastEpisodeIndex + 1 === item.episodeIndex
+        currentEpisodeBlock &&
+        currentEpisodeBlock.id === item.seasonId &&
+        currentEpisodeBlock.lastEpisodeIndex + 1 === item.episodeIndex
       ) {
-        if (currentEpisodeBlock.id === item.seasonId) {
-          currentEpisodeBlock.episodes.push(item);
-          currentEpisodeBlock.lastEpisodeIndex = item.episodeIndex;
-        } else {
-          // grouped.push(currentEpisodeBlock);
-
-          let isHashed = false;
-
-          hashedEpisodeBlocks.forEach((hashed) => {
-            if (item.seasonId === hashed.id) {
-              hashed.episodes.push(item);
-              hashed.lastEpisodeIndex = item.episodeIndex;
-              isHashed = true;
-            }
-          });
-
-          if (!isHashed) {
-            hashedEpisodeBlocks.push(currentEpisodeBlock);
-
-            currentEpisodeBlock = {
-              type: "episodeBlock",
-              id: item.seasonId,
-              name: item.seasonName,
-              commonData: item.commonData,
-              episodes: [item],
-              lastEpisodeIndex: item.episodeIndex,
-            };
-            grouped.push(currentEpisodeBlock);
-          }
-        }
+        currentEpisodeBlock.episodes.push(item);
+        currentEpisodeBlock.lastEpisodeIndex = item.episodeIndex;
       } else {
         // Start a new episode block
         currentEpisodeBlock = {
@@ -135,24 +110,7 @@ function getAllTitlesReleaseOrder() {
       }
     } else {
       // Push any current block before standalone item
-      if (currentEpisodeBlock != null && item.split) {
-        if (item.split === currentEpisodeBlock.id) {
-          currentEpisodeBlock = null;
-        } else {
-          let toRemove = null;
-
-          hashedEpisodeBlocks.forEach((hashed) => {
-            if (item.split === hashed.id) toRemove = hashed;
-          });
-
-          if (toRemove != null) {
-            hashedEpisodeBlocks = hashedEpisodeBlocks.filter(
-              (hashed) => hashed.id !== toRemove.id
-            );
-            console.log(hashedEpisodeBlocks);
-          }
-        }
-      }
+      currentEpisodeBlock = null;
       grouped.push(item);
     }
   }
